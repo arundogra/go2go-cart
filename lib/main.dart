e53907e40a46a7110147f78ea0eafb8b6abdac87
Kakashi <sirakarta22@gmail.com>
2022-07-23 06:57:05 +0530

    setup dependencies again

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,37 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:go2go_cart/view/app/homepage.dart';
+import 'package:go2go_cart/view/widgets/progress_alert.dart';
+import 'view/helpers/colors.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.white));
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
+
+  ProgressView.loadingIndicator();
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        canvasColor: white,
       ),
-      home: AnimatedSplashScreen(
-        splash: Image.asset('assets/icon.png', fit: BoxFit.cover,),
-        nextScreen: MyHomePage(title: 'Flutter Demo Home Page'),
-        duration: 1000,
-        splashTransition: SplashTransition.fadeTransition,
-        backgroundColor: Colors.greenAccent.shade400,
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      builder: EasyLoading.init(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
--- lib/view/widgets/alert_widget.dart
+++ lib/view/widgets/alert_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
 import '../helpers/colors.dart';
 
 class CustomAlert extends StatelessWidget{
@@ -14,7 +12,7 @@ class CustomAlert extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return AlertDialog(
-      title: new Text(txt, style: GoogleFonts.ubuntu(color: darkGrey, fontSize: 14),),
+      title: Text(txt, style: TextStyle(color: darkGrey, fontSize: 14),),
       actions: actions
     );
   }
